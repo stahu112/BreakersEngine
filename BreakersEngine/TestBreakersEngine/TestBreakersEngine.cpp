48f4df1a31f658bc307157571596b256d70afbed
michalStankiewicz <michal.stankicz@o2.pl>
2018-04-10 02:21:51 +0200

    Google tests work successfully

---
+++ BreakersEngine/TestBreakersEngine/TestBreakersEngine.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+//#include "../BreakersEngine/System/Settings.h"
+
+TEST(SquareTests, Square)
+{
+	//BE::System::Settings set{};
+
+	EXPECT_EQ(60, 1);//set.renderSettings.fpsLimit);
+	EXPECT_EQ(30, 30);//set.renderSettings.fpsLimit);
+}
