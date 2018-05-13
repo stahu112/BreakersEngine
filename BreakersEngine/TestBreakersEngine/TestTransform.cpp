8b27168a8dcbb7bad55bbbb1a7ad53224039aa98
michalStankiewicz <michal.stankicz@o2.pl>
2018-05-13 10:52:37 +0200

    Transform tests

--- BreakersEngine/TestBreakersEngine/TestTransform.cpp
+++ BreakersEngine/TestBreakersEngine/TestTransform.cpp
@@ -17,3 +17,35 @@ TEST_F(TestTransform, TestBasicVectors)
 	ASSERT_EQ(sf::Vector2f(0, 1), t.down());
 	ASSERT_EQ(sf::Vector2f(0, -1), t.up());
 }
+
+TEST_F(TestTransform, RotatateWorkProperly)
+{
+
+	t.setRotation(50);
+	ASSERT_EQ(t.getRotation(), 50);
+
+	auto rot = t.getRotation();
+	t.rotate(90);
+	ASSERT_EQ(t.getRotation(), rot + 90);
+}
+
+TEST_F(TestTransform, TranslateWorkProperly)
+{
+
+	t.setPosition(sf::Vector2f(100, 100));
+	ASSERT_EQ(t.getPosition(), sf::Vector2f(100, 100));
+
+	auto pos = t.getPosition();
+	t.translate(sf::Vector2f(10, 10));
+	ASSERT_EQ(t.getPosition(), pos + sf::Vector2f(10, 10));
+}
+
+TEST_F(TestTransform, ScaleWorkProperly)
+{
+	t.setScale(sf::Vector2f(2, 0.5));
+	ASSERT_EQ(t.getScale(), sf::Vector2f(2, 0.5));
+
+	auto scale = t.getScale();
+	t.setScale(sf::Vector2f(0.5, 2));
+	ASSERT_EQ(t.getScale(), sf::Vector2f(0.5, 2));
+}
